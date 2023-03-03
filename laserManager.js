5f37a1ec1308a2946bc3315bfda8f5cf17fb5901
Derek Baert~ <derek.baert@gmail.com>
2023-03-03 11:16:48 -0600

    Refactored code
    Refactored handler functions into classes. Player laser's not hitting saucers.

---
+++ laserManager.js
@@ -0,0 +1,85 @@
+class LaserManager
+{
+    constructor()
+    {
+        this.lasers = [];   
+    }
+
+    handleLasers()
+    {
+        for(let i = this.lasers.length - 1; i >= 0; i--)
+        {
+            if(!this.lasers[i].alive())
+            {
+                this.lasers.splice(i,1);
+            }
+            else
+            {
+                this.lasers[i].display(); 
+                this.lasers[i].update();
+                this.lasers[i].checkEdges();
+                for(let j = asteroidManager.asteroids.length - 1; j >=0; j--)
+                {
+                    if(this.lasers[i].hits(asteroidManager.asteroids[j]))
+                    {
+                        soundManager.asteroidPlay();
+                        if(asteroidManager.asteroids[j].size == 40)
+                        {
+                            score += 20;
+                        }
+                        else if(asteroidManager.asteroids[j].size >= 20)
+                        {
+                            score += 50;
+                        }
+                        else
+                        {
+                            score += 100;
+                        }
+                        let newAsteroids = asteroidManager.asteroids[j].break();
+                        asteroidManager.asteroids = asteroidManager.asteroids.concat(newAsteroids);
+                        asteroidManager.asteroids.splice(j, 1);
+                        this.lasers.splice(i,1);
+                        break;
+                    }
+                }            
+            }    
+        }
+
+        for(let i = this.lasers.length - 1; i >= 0; i--)
+        {
+            for(let j = saucerManager.saucers.length - 1; j >=0; j--)
+            {
+                if(this.lasers[i].hits(saucerManager.saucers[j]) && this.lasers[i].laserType != LaserType.Enemy)
+                {
+                    soundManager.explodePlay();
+                    if(saucerManager.saucers[j].size == 60)
+                    {
+                        score += 200;
+                    }
+                    else if(saucerManager.saucers[j].size == 30)
+                    {
+                        score += 1000;
+                    }
+                    saucerManager.saucers.splice(i, 1);
+                }
+            }
+            if(this.lasers[i].hits(ship) && this.lasers[i].laserType != LaserType.Player)
+            {
+                soundManager.explodePlay();
+                this.lasers.splice(i, 1);
+                lives--;
+                ship.respawn();
+            }
+        }
+    }
+
+    add(position, heading, type)
+    {
+        this.lasers.push(new Laser(position, heading, type));
+    }
+
+    reset()
+    {
+        this.lasers = [];
+    }
+}
---
+++ saucerManager.js
@@ -0,0 +1,57 @@
+class SaucerManager
+{
+    constructor()
+    {
+        this.saucers = [];
+    }
+
+    update(soundManager)
+    {
+        if(this.saucers.length > 0)
+        {
+            soundManager.saucerPlay();
+        }
+        else
+        {
+            soundManager.saucerStop();
+        }
+    }
+
+    handleSaucers()
+    {
+        for (let i = 0; i < this.saucers.length; i++) 
+        {
+          if (this.saucers[i].checkEdges()) 
+          {
+            this.saucers.splice(i, 1);
+          }
+          else {
+            this.saucers[i].display();
+            this.saucers[i].update();
+      
+            if (ship.hits(this.saucers[i])) 
+            {
+              soundManager.explodePlay();
+              lives--;
+              ship.respawn();
+            }
+      
+            if (frameCount % 60 === 0) 
+            {
+              soundManager.laserPlay();
+              laserManager.add(this.saucers[i].position, this.saucers[i].heading, LaserType.Enemy);
+            }
+          }
+        }
+    }
+
+    reset()
+    {
+        this.saucers = [];
+    }
+
+    add(saucerSize)
+    {
+        this.saucers.push(new Saucer(saucerSize));
+    }
+}
